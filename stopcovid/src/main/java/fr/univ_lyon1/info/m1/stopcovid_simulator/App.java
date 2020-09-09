916df5402829f9fc04172bbded4101b56e7e3311
BESSAADI ILYESS p1707782 <ilyess.bessaadi@etu.univ-lyon1.fr>
2020-09-09 10:19:37 +0200

    Avancement du MVC

--- stopcovid/src/main/java/fr/univ_lyon1/info/m1/stopcovid_simulator/App.java
+++ stopcovid/src/main/java/fr/univ_lyon1/info/m1/stopcovid_simulator/App.java
@@ -1,7 +1,33 @@
 package fr.univ_lyon1.info.m1.stopcovid_simulator;
 
-public class App {
-	public static void main(String[] _args) {
+import fr.univ_lyon1.info.m1.stopcovid_simulator.controller.MainController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-	}
+import java.io.IOException;
+
+public class App extends Application {
+    @Override
+    public void start(final Stage mainStage) throws Exception {
+        mainStage.setTitle("Stop COVID");
+//        mainStage.setScene(CreateMainScene());
+        mainStage.show();
+
+        System.out.println(getClass().getResource("/fr/univ_lyon1/info/m1/stopcovid_simulator/view"));
+    }
+
+    public static void main(final String[] args) {
+        launch(args);
+    }
+
+    private Scene CreateMainScene() throws IOException {
+        var mainController = new MainController();
+
+        var mainFXMLLoader = new FXMLLoader(getClass().getResource("/fr/univ_lyon1/info/m1/stopcovid_simulator/view/MainView.fxml"));
+        mainFXMLLoader.setController(mainController);
+
+        return new Scene(mainFXMLLoader.load());
+    }
 }
---
+++ stopcovid/src/main/java/fr/univ_lyon1/info/m1/stopcovid_simulator/controller/MainController.java
@@ -0,0 +1,13 @@
+package fr.univ_lyon1.info.m1.stopcovid_simulator.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
