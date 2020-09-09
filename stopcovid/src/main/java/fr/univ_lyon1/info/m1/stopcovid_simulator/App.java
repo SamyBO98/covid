1ff8cc7584d9ede882da46a03b607231fc02ba76
BESSAADI ILYESS p1707782 <ilyess.bessaadi@etu.univ-lyon1.fr>
2020-09-09 11:26:30 +0200

    Récupération des ressources fxml

--- stopcovid/src/main/java/fr/univ_lyon1/info/m1/stopcovid_simulator/App.java
+++ stopcovid/src/main/java/fr/univ_lyon1/info/m1/stopcovid_simulator/App.java
@@ -12,11 +12,8 @@ public class App extends Application {
     @Override
     public void start(final Stage mainStage) throws Exception {
         mainStage.setTitle("Stop COVID");
-//        mainStage.setScene(CreateMainScene());
+        mainStage.setScene(createMainScene());
         mainStage.show();
-
-        System.out.println(getClass()
-                .getResource("/fr/univ_lyon1/info/m1/stopcovid_simulator/view"));
     }
 
     /**
@@ -33,7 +30,7 @@ public class App extends Application {
         var mainController = new MainController();
 
         var mainFXMLLoader = new FXMLLoader(getClass()
-                .getResource("/fr/univ_lyon1/info/m1/stopcovid_simulator/view/MainView.fxml"));
+                .getResource("/view/MainView.fxml"));
         mainFXMLLoader.setController(mainController);
 
         return new Scene(mainFXMLLoader.load());
