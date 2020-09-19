69618f22dc5dc79b2589eba8bbf2c6c127f60090
BEN OTHMAN SAMY p1707136 <samy.ben-othman@etu.univ-lyon1.fr>
2020-09-19 15:31:13 +0200

    Delete useless import

--- stopcovid/src/test/java/fr/univ_lyon1/info/m1/stopcovid_simulator/model/ClientModelTest.java
+++ stopcovid/src/test/java/fr/univ_lyon1/info/m1/stopcovid_simulator/model/ClientModelTest.java
@@ -1,6 +1,5 @@
 package fr.univ_lyon1.info.m1.stopcovid_simulator.model;
 
-import com.github.hervian.reflection.Event;
 import fr.univ_lyon1.info.m1.stopcovid_simulator.util.enums.Status;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
