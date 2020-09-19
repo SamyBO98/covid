4af512a0309caed70bf76f121f047f2f74988d4a
BESSAADI ILYESS p1707782 <ilyess.bessaadi@etu.univ-lyon1.fr>
2020-09-19 22:19:02 +0200

    Change simulated time

--- stopcovid/src/main/java/fr/univ_lyon1/info/m1/stopcovid_simulator/model/SimulatedTime.java
+++ stopcovid/src/main/java/fr/univ_lyon1/info/m1/stopcovid_simulator/model/SimulatedTime.java
@@ -1,9 +1,9 @@
 package fr.univ_lyon1.info.m1.stopcovid_simulator.model;
 
 public final class SimulatedTime {
-    public static final int DAY = 5000; //In milliseconds
+    public static final int DAY = 1000; //In milliseconds
     public static final int FORTNIGHTLY = 14 * DAY;
-    public static final int UPDATE_ID_DELAY = DAY;
+    public static final int UPDATE_ID_DELAY = 7 * DAY;
 
     private SimulatedTime() {
     }
